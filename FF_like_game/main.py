cdfab787a5b65528dfe100bcdd8ebacdea80f7b1
Cybernetic Ransomware <aleksander.marszakiewicz.pp@gmail.com>
2021-12-14 21:50:34 +0100

    Append background image and space for toolbar.

--- FF_like_game/main.py
+++ FF_like_game/main.py
@@ -3,11 +3,20 @@ import pygame
 
 pygame.init()
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 400
+
+SCREEN_WIDTH = 1124
+SCREEN_HEIGHT = SCREEN_WIDTH // (800/600)
+BOTTOM_PANEL = SCREEN_HEIGHT - 725
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Battle Time!')
+
+background_img = pygame.image.load('img/cyberpunk-gf755e0a93_1280.png').convert_alpha()
+
+
+def draw_background():
+    screen.blit(background_img, (0, 0))
+
 
 if __name__ == '__main__':
     pygame.display.set_caption('Battle Time!')
@@ -15,8 +24,12 @@ if __name__ == '__main__':
 
     while run:
 
+        draw_background()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
+        pygame.display.update()
 
     pygame.quit()
